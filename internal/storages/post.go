296342bedaa8f6b321ef1ce519ea081636875c94
pringleskate <kattyseskimojoe@gmail.com>
2020-12-14 18:28:51 +0300

    Smth

--- internal/storages/post.go
+++ internal/storages/post.go
@@ -6,9 +6,9 @@ import (
 )
 
 type PostStorage interface {
-	InsertSomePosts()
-	InsertPost(postInput models.Post) (err error)
-	UpdatePost()
+	InsertSomePosts(inputPosts []models.Post) (posts []models.Post, err error)
+	InsertPost(postInput models.Post) (ID int, err error)
+	UpdatePost(postInput models.PostUpdate) (err error)
 	GetFullPost()
 	GetAllPostsByThread()
 }
@@ -23,16 +23,33 @@ func NewPostStorage(db *pgx.ConnPool) PostStorage{
 	}
 }
 
-func (p postStorage) InsertSomePosts() {
-	panic("implement me")
+func (p postStorage) InsertSomePosts(inputPosts []models.Post) (posts []models.Post, err error) {
+	for i, post := range inputPosts {
+		ID, err := p.InsertPost(post)
+		if err != nil {
+			return
+		}
+
+		inputPosts[i].ID = ID
+	}
+
+	return inputPosts, nil
 }
 
-func (p postStorage) InsertPost(postInput models.Post) (err error) {
+func (p postStorage) InsertPost(postInput models.Post) (ID int, err error) {
+	if postInput.Parent == 0 {
+		err = p.db.QueryRow("INSERT INTO post (author, created, forum, message, parent, thread, path) VALUES ($1,$2,$3,$4,$5,$6, array[(select currval('post_id_seq')::integer)]) RETURNING ID",
+			postInput.Author, postInput.Created, postInput.Forum, postInput.Message, postInput.Parent, postInput.ThreadSlagOrID.ThreadID).Scan(&ID)
+
+	} else {
+		err = p.db.QueryRow("INSERT INTO post (author, created, forum, message, parent, thread, path) VALUES ($1,$2,$3,$4,$5,$6, (SELECT path FROM post WHERE id = $5) || (select currval('post_id_seq')::integer)) RETURNING ID",
+			postInput.Author, postInput.Created, postInput.Forum, postInput.Message, postInput.Parent, postInput.ThreadSlagOrID.ThreadID).Scan(&ID)
+	}
 	return
 }
 
-func (p postStorage) UpdatePost() {
-	panic("implement me")
+func (p postStorage) UpdatePost(postInput models.PostUpdate) (err error) {
+
 }
 
 func (p postStorage) GetFullPost() {
